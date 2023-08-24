6801b5f0eb573396501956613184793fd19f00e8
Lucasch <plastikman@tlen.pl>
2023-08-24 18:39:42 +0200

    finder2

--- frontend/src/components/RouteFinder.tsx
+++ frontend/src/components/RouteFinder.tsx
@@ -9,24 +9,27 @@ interface RoutesFinderProps {
   setFilteredRoutes: (Routes: Route[]) => void;
 }
 
+type DefaultState = {
+  name: string;
+  rockType: string;
+  country: string;
+  rate: string;
+};
 
 const RoutesFinder = ({ setFilteredRoutes }: RoutesFinderProps) => {
+  const inputElement = useRef<string>();
   const { routes } = useLoaderData() as CountriesDetailPageRouteData;
 
-  const [state, setState] = useState<any>({
+  const optionsValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Dobra praktyka, często wkłada się w stan jeżeli jest potrzeba
+
+  const [selectedValues, setSelectedValues] = useState<any>({
     name: "",
     rockType: "",
     country: "",
     rate: "",
-  });
+  }); // Tutaj stan potrzebny, bo chcemy wyświetlić dane w kontencie
+  console.log(selectedValues, "selectedValues");
 
-  // const [rockType, setRockType] = useState<string | undefined>();
-  // const [country, setCountry] = useState<string | undefined>();
-  // const [name, setName] = useState<string | undefined>();
-  // const [rate, setRate] = useState<string | undefined>();
-
-  console.log(state, "state");
-  // console.log(rate, "rate");
   const rockCategory = Array.from(
     new Set(routes.map((route) => route.tags["climbing:rock"]))
   );
@@ -37,21 +40,45 @@ const RoutesFinder = ({ setFilteredRoutes }: RoutesFinderProps) => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(e.target, "e");
-    // setState({
-    //   name: e.target[0].value,
-    //   rockType: e.target[1].value,
-    //   country: e.target[2].value,
-    //   rate: e.target[3].value,
-    // });
+    const formElements = [...e.currentTarget.elements];
+    console.log(formElements, "formElements");
 
-    console.log(e.currentTarget.name, "name");
+    const selectNameElement = formElements.find(
+      (formElement) => formElement.id === "name"
+    )! as HTMLSelectElement;
+
+    const selectedInputNameElements = [selectNameElement.value];
+
+    setSelectedValues((prevState:any) => ({
+      ...prevState,
+      name: selectedInputNameElements,
+    }));
+
+    const selectRockTypeElements = formElements.find(
+      (formElement) => formElement.id === "rockType"
+    )! as HTMLSelectElement;
+
+    const selectedRockTypeElements = [selectRockTypeElements.value];
+
+    setSelectedValues((prevState:any) => ({
+      ...prevState,
+      rockType: selectedRockTypeElements,
+    }));
+    // console.log(e.target[0].value);
+    // console.log(e.target.elements.name.value);
+    // console.log(e.target.name.value);
+
+    // const data = new FormData(e.target);
+
+    // console.log(data.get("country"));
+    // console.log(data.get("name"));
+    // console.log(e.target[3].value, "e.currentTarget.value");
   };
 
   useEffect(() => {
     setFilteredRoutes(
       routes.filter((route) => {
-        const { rockType, country, name, rate } = state;
+        const { rockType, country, name, rate } = selectedValues;
         return (
           (!rockType || rockType === route.tags["climbing:rock"]) &&
           (!country || country === route.country[0]) &&
@@ -61,20 +88,16 @@ const RoutesFinder = ({ setFilteredRoutes }: RoutesFinderProps) => {
         );
       })
     );
-  }, [state.country, state.rockType, state.name]);
+  }, [selectedValues.country, selectedValues.rockType, selectedValues.name]);
 
   const clearFilters = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setState({
+    setSelectedValues({
       name: "",
       rockType: "",
       country: "",
       rate: "",
     });
-
-    // setCountry("");
-    // setRockType("");
-    // setName("");
   };
 
   return (
@@ -82,6 +105,7 @@ const RoutesFinder = ({ setFilteredRoutes }: RoutesFinderProps) => {
       <h1>Gym Finder</h1>
       <form onSubmit={handleSubmit}>
         <input name="name" type="text" id="name"></input>
+
         <label htmlFor="rockType"></label>
         <select name="rockType" id="rockType">
           <option value="">Any</option>
@@ -91,14 +115,16 @@ const RoutesFinder = ({ setFilteredRoutes }: RoutesFinderProps) => {
         </select>
 
         <label htmlFor="country"></label>
-        <select name="country" id="country" value={state.country}>
+        <select name="country" id="country">
           <option value="">Any</option>
           <option value="POL">Poland</option>
           <option value="ITA">Italy</option>
           <option value="DEU">Deu</option>
         </select>
+
         <label htmlFor="rate"></label>
         <select name="rate" id="rate">
+          <option value="">Any</option>
           <option value="1">1 of 10 stars</option>
           <option value="2">2 of 10 stars</option>
           <option value="3">3 of 10 stars</option>
